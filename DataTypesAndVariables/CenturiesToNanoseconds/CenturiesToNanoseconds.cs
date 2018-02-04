ea1f48deb4e8150137fdbc83eca5740cc75c6056
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:18:55 +0200

    Add files via upload

---
+++ DataTypesAndVariables/CenturiesToNanoseconds/CenturiesToNanoseconds.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CenturiesToNanoseconds
+{
+    class CenturiesToNanoseconds
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            double hours = days * 24;
+            double minutes = hours * 60;
+            double seconds = minutes * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {seconds}000 milliseconds = {seconds}000000 microseconds = {seconds}000000000 nanoseconds");
+
+        }
+    }
+}
