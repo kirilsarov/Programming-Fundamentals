07195887effc99f85e0a881183f7b26a77c6739c
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:19:31 +0200

    Add files via upload

---
+++ DataTypesAndVariables/ComparingFloats/ComparingFloats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ComparingFloats
+{
+    class ComparingFloats
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double eps = 0.000001;
+            double difference = Math.Abs(num1 - num2);
+            if (difference >= eps)
+            {
+                Console.WriteLine("False");
+            }
+            else if (difference < eps)
+            {
+                Console.WriteLine("True");
+            }
+        }
+    }
+}
