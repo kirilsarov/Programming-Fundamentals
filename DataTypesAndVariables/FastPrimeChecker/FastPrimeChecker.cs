c985da522295f14f7360e4698fdb283b6b63a196
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:24:10 +0200

    Add files via upload

---
+++ DataTypesAndVariables/FastPrimeChecker/FastPrimeChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FastPrimeChecker
+{
+    class FastPrimeChecker
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int current = 2; current <= number; current++)
+            {
+                bool isItPrime = true;
+                for (int delio = 2; delio <= Math.Sqrt(current); delio++)
+                {
+                    if (current % delio == 0)
+                    {
+                        isItPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{current} -> {isItPrime}");
+            }
+
+        }
+    }
+}
