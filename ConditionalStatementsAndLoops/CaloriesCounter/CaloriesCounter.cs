5964a920eca08bfec561018efe4df76520021442
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:06:00 +0200

    Add files via upload

---
+++ ConditionalStatementsAndLoops/CaloriesCounter/CaloriesCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CaloriesCounter
+{
+    class CaloriesCounter
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string ingredient;
+            int calories = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                ingredient = Console.ReadLine().ToLower();
+                if (ingredient == "cheese")
+                {
+                    calories += 500;
+                }
+                else if (ingredient == "tomato sauce")
+                {
+                    calories += 150;
+                }
+                else if (ingredient == "salami")
+                {
+                    calories += 600;
+                }
+                else if (ingredient == "pepper")
+                {
+                    calories += 50;
+                }
+            }
+            Console.WriteLine($"Total calories: {calories}");
+        }
+    }
+}
