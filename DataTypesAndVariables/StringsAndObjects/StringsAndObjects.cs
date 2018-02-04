8ac43b7496a8222545678f335a011836600564b1
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:30:41 +0200

    Add files via upload

---
+++ DataTypesAndVariables/StringsAndObjects/StringsAndObjects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringsAndObjects
+{
+    class StringsAndObjects
+    {
+        static void Main(string[] args)
+        {
+            string strOne = Console.ReadLine();
+            string strTwo = Console.ReadLine();
+            object sum = strOne + " " + strTwo;
+            Console.WriteLine(sum);
+        }
+    }
+}
