dba8a2d6acdfb78991fcb586530665fcf8abadf9
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:31:12 +0200

    Add files via upload

---
+++ DataTypesAndVariables/VariablesInHexFormat/VariablesInHexFormat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariablesInHexFormat
+{
+    class VariablesInHexFormat
+    {
+        static void Main(string[] args)
+        {
+            string hexa = Console.ReadLine();
+            int number = Convert.ToInt32(hexa, 16);
+            Console.WriteLine(number);
+        }
+    }
+}
