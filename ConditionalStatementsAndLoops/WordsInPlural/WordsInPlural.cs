af6cf93a3b621ea92095a8c7c0b41b9d93b4b9ac
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:15:31 +0200

    Add files via upload

---
+++ ConditionalStatementsAndLoops/WordsInPlural/WordsInPlural.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WordsInPlural
+{
+    class WordsInPlural
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            if (word.EndsWith("y"))
+            {
+                word = word.Remove(word.Length - 1);
+                word = word + "ies";
+                Console.WriteLine(word);
+            }
+            else if (word.EndsWith("o") || word.EndsWith("ch") || word.EndsWith("s") || word.EndsWith("sh") || word.EndsWith("x") || word.EndsWith("z"))
+            {
+                word = word + "es";
+                Console.WriteLine(word);
+            }
+            else
+            {
+                word = word + "s";
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
