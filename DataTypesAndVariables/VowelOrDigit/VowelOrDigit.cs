69313da7c4c29bd7493c8c3041621bc6e03d6e78
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:32:09 +0200

    Add files via upload

---
+++ DataTypesAndVariables/VowelOrDigit/VowelOrDigit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VowelOrDigit
+{
+    class VowelOrDigit
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            if (input == 97 || input == 101 || input == 105 || input == 111 || input == 117 || input == 121)
+            {
+                Console.WriteLine("vowel");
+            }
+            else if (input >= 48 && input <= 57)
+            {
+                Console.WriteLine("digit");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
