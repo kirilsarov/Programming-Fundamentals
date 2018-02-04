a3f1704b0b1b3130d92a07edc6dfa882ec68ff11
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:25:51 +0200

    Add files via upload

---
+++ DataTypesAndVariables/PracticeCharAndString/PracticeCharAndString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PracticeCharAndString
+{
+    class PracticeCharAndString
+    {
+        static void Main(string[] args)
+        {
+            string strOne = "Software University";
+            char chrOne = 'B';
+            char chrTwo = 'y';
+            char chrThree = 'e';
+            string strTwo = "I love programming";
+
+            Console.WriteLine(strOne);
+            Console.WriteLine(chrOne);
+            Console.WriteLine(chrTwo);
+            Console.WriteLine(chrThree);
+            Console.WriteLine(strTwo);
+        }
+    }
+}
