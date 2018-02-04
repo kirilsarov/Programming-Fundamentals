d2a042e8baca8b741483dbdffc6305ddd44e016e
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:18:03 +0200

    Add files via upload

---
+++ DataTypesAndVariables/BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BooleanVariable
+{
+    class BooleanVariable
+    {
+        static void Main(string[] args)
+        {
+            string boolean = Console.ReadLine();
+            bool isItTrue = Convert.ToBoolean(boolean);
+            if (isItTrue == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (isItTrue == false)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
