71602821cd4bf124fb646aa0b97c1597c2cc12cf
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:06:56 +0200

    Add files via upload

---
+++ ConditionalStatementsAndLoops/ChooseaDrink/ChooseaDrink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChooseaDrink
+{
+    class ChooseaDrink
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine().ToLower();
+            if (profession == "athlete")
+            {
+                Console.WriteLine("Water");
+            }
+            else if (profession == "businessman" || profession == "businesswoman")
+            {
+                Console.WriteLine("Coffee");
+            }
+            else if (profession == "softuni student")
+            {
+                Console.WriteLine("Beer");
+            }
+            else
+            {
+                Console.WriteLine("Tea");
+            }
+        }
+    }
+}
