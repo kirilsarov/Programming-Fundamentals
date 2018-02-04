19e6963a07c6f855aaa60bd358b16d775b2a3975
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:29:13 +0200

    Add files via upload

---
+++ DataTypesAndVariables/ReversedChars/ReversedChars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReversedChars
+{
+    class ReversedChars
+    {
+        static void Main(string[] args)
+        {
+            string chrOne = (Console.ReadLine());
+            string chrTwo = (Console.ReadLine());
+            string chrThree = (Console.ReadLine());
+
+            Console.WriteLine(chrThree + chrTwo + chrOne);
+        }
+    }
+}
