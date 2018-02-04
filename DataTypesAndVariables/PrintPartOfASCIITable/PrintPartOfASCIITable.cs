6f817c3ea86815e6b073dbf0b0bb4c24c49ad781
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:27:45 +0200

    Add files via upload

---
+++ DataTypesAndVariables/PrintPartOfASCIITable/PrintPartOfASCIITable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrintPartOfASCIITable
+{
+    class PrintPartOfASCIITable
+    {
+        static void Main(string[] args)
+        {
+            int chrOne = int.Parse(Console.ReadLine());
+            int chrTwo = int.Parse(Console.ReadLine());
+            for (char i = (char)chrOne; i <= (char)chrTwo; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
