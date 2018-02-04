8c66790f013b47c298f20bc6a22fffdc1bfc5905
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:08:19 +0200

    Add files via upload

---
+++ ConditionalStatementsAndLoops/CountTheIntegers/CountTheIntegers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountTheIntegers
+{
+    class CountTheIntegers
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            try
+            {
+                while (true)
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    counter++;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
