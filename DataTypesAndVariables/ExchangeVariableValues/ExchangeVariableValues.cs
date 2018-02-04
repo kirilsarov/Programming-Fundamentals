5a5c1ef26e01415a1f4b8ac12ff46ff791bbd5f0
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:23:38 +0200

    Add files via upload

---
+++ DataTypesAndVariables/ExchangeVariableValues/ExchangeVariableValues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExchangeVariableValues
+{
+    class ExchangeVariableValues
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+}
