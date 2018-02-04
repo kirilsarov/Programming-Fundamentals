4ebc7e988bb67c6c2aa56423b354ae3e9e40752c
Kiril Sarov <35762867+kirilsarov@users.noreply.github.com>
2018-02-04 13:03:42 +0200

    Add files via upload

---
+++ ConditionalStatementsAndLoops/CakeIngredients/CakeIngredients.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CakeIngredients
+{
+    class CakeIngredients
+    {
+        static void Main(string[] args)
+        {
+            string ingredient;
+            for (int i = 1; i <= 21 ; i++)
+            {
+                ingredient = Console.ReadLine();
+                if (ingredient != "Bake!")
+                {
+                    Console.WriteLine($"Adding ingredient {ingredient}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Preparing cake with {i-1} ingredients.");
+                    break;
+                }
+            }
+        }
+    }
+}
